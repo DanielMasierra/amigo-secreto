9851e9b85b1feb014f93cd150abfb0f5af384e66
Daniel Macias <jmacias@comunidad.unam.mx>
2025-08-20 13:07:55 -0600

    Declara Array Inicial para almacenar amigos.

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//A continuación, el array inicial para almacenar amigos
+let amigos = [];
+
